bde1065bcba1b2ef324ae36f4975c8d95e30a85a
Mudit Khanna <muditkhanna998@gmail.com>
2026-04-03 01:04:17 -0400

    Initial chat service setup

---
+++ src/main/java/com/mudit/piingochatservice/PiingoChatServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mudit.piingochatservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiingoChatServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiingoChatServiceApplication.class, args);
+    }
+
+}
